db8d0a22b6429f4b26be76524352206a8a5c4a71
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2025-04-03 11:04:55 +0200

    User can logout now. Will work on the chatfunction know.

--- src/main/java/com/example/scrumapppp/Controller/TeamController.java
+++ src/main/java/com/example/scrumapppp/Controller/TeamController.java
@@ -2,8 +2,14 @@ package com.example.scrumapppp.Controller;
 import com.example.scrumapppp.Session.UserSession;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class TeamController {
     @FXML
@@ -13,4 +19,40 @@ public class TeamController {
         usernameText.setText("Welkom " + UserSession.getUsername());
         UserSession.getData();
     }
+
+    public void chatButtonClick(ActionEvent event){
+        System.out.println("Chat button clicked!");
+        try {
+            // Load the new FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/scrumapppp/Chat.fxml"));
+            Scene homeScene = new Scene(loader.load());
+
+            // Get the current stage
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+            // Set the new scene
+            stage.setScene(homeScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void logOutButtonClick(ActionEvent event){
+        System.out.println("Register button clicked!");
+        try {
+            UserSession.clearSession();
+            // Load the new FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/scrumapppp/InlogScherm.fxml"));
+            Scene homeScene = new Scene(loader.load());
+
+            // Get the current stage
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+            // Set the new scene
+            stage.setScene(homeScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
