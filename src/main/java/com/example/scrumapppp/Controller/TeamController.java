6a60996dc98dbdfa5d7775f83c0a1ac2c83f37b6
Robuit <24012645@student.hhs.nl>
2025-04-08 12:35:07 +0200

    ni

--- src/main/java/com/example/scrumapppp/Controller/TeamController.java
+++ src/main/java/com/example/scrumapppp/Controller/TeamController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;  // Voeg MouseEvent toe
 
 public class TeamController {
 
@@ -20,6 +21,14 @@ public class TeamController {
     private void logOutButtonClick(ActionEvent event) {
         System.out.println("Uitloggen clicked!");
         // Voeg hier de uitloglogica toe
+    }
+
+    // Deze methode wordt aangeroepen wanneer de create-knop wordt geklikt
+    @FXML
+    private void handleCreateButtonClick(ActionEvent event) {
+        System.out.println("Create button clicked!");
+        // Voeg hier de logica toe voor het aanmaken van een Scrumboard
+        showAlert("Scrumboard Gecreëerd", "Het Scrumboard is succesvol aangemaakt!", AlertType.INFORMATION);
     }
 
     // Een eenvoudige alert methode om een bericht te tonen
